00afcfb0cf5d1c3ba3206a23f9a7c9737c30f878
Luca Chiappa <luca.chiappa15@gmail.com>
2026-01-22 10:46:04 +0000

    repository

---
+++ Spring boot/demo/src/main/java/com/example/demo/EnergiaRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.Studente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EnergiaRepository extends JpaRepository<Energia, Long> {
+}
