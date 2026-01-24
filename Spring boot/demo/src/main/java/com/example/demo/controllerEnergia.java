56684caa2f687081aead8740fa442ec052222ceb
Luca Chiappa <luca.chiappa15@gmail.com>
2026-01-24 07:16:53 +0000

    Controller

--- Spring boot/demo/src/main/java/com/example/demo/controllerEnergia.java
+++ Spring boot/demo/src/main/java/com/example/demo/controllerEnergia.java
@@ -1,6 +1,31 @@
+@RestController
+@RequestMapping("/energia")
+
 public class ControllerEnergia{
 
-    public EnergiaController(EnergiaRepository energiaService){
+    private final energiaService energiaService;
 
+    public EnergiaController(EnergiaRepository energiaService){
+        this.energiaService = energiaService;
+    }
+
+    @GetMapping
+    public List<Energia> getAll(){
+        return energiaService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Energia getById(@PathVariable Long id){
+        return energiaService.findById(id);
+    }
+
+    @PostMapping
+    public Energia create(@RequestMapping Energia e){
+        return energiaService.salva(e);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id){
+        energiaService.delete(id);
     }
 }
